5d8094d6539b90c0a060be3874d17c3e3aa81064
Via Vianca Contreras Colque <vi.122494@gmail.com>
2023-06-12 19:13:29 -0400

    Add store and update request

---
+++ app/Http/Requests/UpdateUserRequest.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace App\Http\Requests;
+
+use Illuminate\Foundation\Http\FormRequest;
+
+class UpdateUserRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     */
+    public function authorize(): bool
+    {
+        return true;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array<string, \Illuminate\Contracts\Validation\ValidationRule|array|string>
+     */
+    public function rules(): array
+    {
+        return [
+            'name'             => 'required|string|max:255',
+            'paternal_surname' => 'required_without:maternal_surname|string|max:255',
+            'maternal_surname' => 'required_without:paternal_surname|string|max:255',
+            'username'         => 'required|string|max:255|unique:App\Models\User,username,'.$this->user->id,
+            'email'            => 'required|email|max:255|unique:App\Models\User,email,'.$this->user->id,
+            'password'         => 'sometimes|required|confirmed',
+        ];
+    }
+}
