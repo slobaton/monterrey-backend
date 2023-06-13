978abf518af19b87c2df21d7ec9366ffa21bebcb
Via Vianca Contreras Colque <vi.122494@gmail.com>
2023-06-12 22:41:48 -0400

    Add crud test for users

--- tests/Feature/UserTest.php
+++ tests/Feature/UserTest.php
@@ -1,17 +1,31 @@
 <?php
 namespace Tests\Feature;
 
+use App\Models\User;
 use Symfony\Component\HttpFoundation\Response;
+use function Pest\Laravel\getJson;
+use function Pest\Laravel\deleteJson;
 use function Pest\Laravel\postJson;
+use function Pest\Laravel\putJson;
 
-it('should create a user', function () {
+it('should return every user (index)', function () {
+    User::factory()->count(3)->create();
+
+    $users = getJson(route('users.index'))
+        ->assertStatus(Response::HTTP_OK)
+        ->json('data');
+
+    expect($users)->toHaveCount(3);
+});
+
+it('should create a user (store)', function () {
     $user = [
-        'name' => 'Pamela',
-        'paternal_surname' => 'Rocha',
+        'name'             => 'Sergio',
+        'paternal_surname' => 'Lobaton',
         'maternal_surname' => 'Arcani',
-        'email' => 'pamela.0722@gmail.com',
-        'username' => 'procha',
-        'password' => 'sergio123!',
+        'email'            => 'lobaton.0722@gmail.com',
+        'username'         => 'slobaton',
+        'password'         => 'sergio123!',
         'password_confirmation' => 'sergio123!'
     ];
     $response = postJson(route('users.store'), $user)
@@ -21,3 +35,57 @@ it('should create a user', function () {
         ->message->toBe('User has been created');
 });
 
+it('should update an existing user (update)', function () {
+
+    $user = User::factory()->create();
+
+    $newData = [
+        'name'             => 'Sergio',
+        'paternal_surname' => 'Lobaton',
+        'maternal_surname' => 'Arcani',
+        'username'         => $user->username,
+        'email'            => $user->email
+    ];
+
+    $updatedUser = putJson(
+        route('users.update', compact('user')),
+        $newData,
+    )
+        ->assertStatus(Response::HTTP_OK)
+        ->json('data');
+
+    expect($updatedUser)
+        ->name->toBe($newData['name'])
+        ->paternal_surname->toBe($newData['paternal_surname'])
+        ->maternal_surname->toBe($newData['maternal_surname'])
+        ->username->toBe($user->username)
+        ->email->toBe($user->email);
+});
+
+it('should return a user (show)', function () {
+    $user = User::factory([
+        'name'             => 'Pamela',
+        'paternal_surname' => 'Rocha',
+        'maternal_surname' => 'Arcani',
+        'email'            => 'pamela.0722@gmail.com',
+        'username'         => 'procha'
+    ])->create();
+
+    $response = getJson(route('users.show', ['user' => $user->id]))->json('data');
+    expect($response)->toMatchArray([
+        'name'             => 'Pamela',
+        'paternal_surname' => 'Rocha',
+        'maternal_surname' => 'Arcani',
+        'email'            => 'pamela.0722@gmail.com',
+        'username'         => 'procha'
+    ]);
+});
+
+it('should delete a user (destroy)', function () {
+    $user = User::factory()->create();
+
+    deleteJson(route('users.destroy', ['user' => $user->id]))
+        ->assertStatus(Response::HTTP_OK);
+
+    $this->assertDatabaseCount('users', 0);
+});
