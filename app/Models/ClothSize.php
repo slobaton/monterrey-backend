56d226debb00d741e196036a6e520ea1f968fbf2
abelopezpaniagua <78995406+abelopezpaniagua@users.noreply.github.com>
2023-07-01 00:13:01 -0400

    implement migration, model and seeders

---
+++ app/Models/ClothSize.php
@@ -0,0 +1,70 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Support\Facades\DB;
+use Spatie\QueryBuilder\AllowedSort;
+use Spatie\QueryBuilder\AllowedFilter;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Builder;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+
+class ClothSize extends Model
+{
+    use HasFactory;
+
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array<int, string>
+     */
+    protected $fillable = [
+        'name',
+        'description',
+        'wash_price',
+        'wash_special_price',
+        'is_active'
+    ];
+
+    protected $casts = [
+        'is_active' => 'boolean',
+    ];
+
+    public function scopeAll(Builder $query, $search): Builder
+    {
+        return $query->where(DB::raw('LOWER(name)'), 'LIKE', "%" . strtolower($search) . "%")
+            ->orWhere(DB::raw('LOWER(description)'), 'LIKE', "%" . strtolower($search) . "%");
+    }
+
+    public static function getAllowedFilters()
+    {
+        return [
+            'name',
+            'description',
+            AllowedFilter::scope('all'),
+        ];
+    }
+
+    public static function getAllowedSorts()
+    {
+        return [
+            'name',
+            'description',
+            'wash_price',
+            'wash_special_price',
+            'is_active',
+            AllowedSort::field('created_at'),
+            AllowedSort::field('updated_at'),
+        ];
+    }
+
+    public static function getDefaultSort(): String
+    {
+        return 'name';
+    }
+
+    public static function getAllowedIncludes(): array
+    {
+        return [];
+    }
+}
