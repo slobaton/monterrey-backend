7722f59a253a983784c4825f9da386c4c2a21919
Sergio Lobaton <sergio.lobaton@endeandina.bo>
2023-07-06 16:50:19 -0400

    Add observer to increment code

--- app/Providers/EventServiceProvider.php
+++ app/Providers/EventServiceProvider.php
@@ -2,13 +2,18 @@
 
 namespace App\Providers;
 
+use App\Models\WashOrder;
+use App\Observers\WashOrderObserver;
+use Illuminate\Support\Facades\Event;
 use Illuminate\Auth\Events\Registered;
 use Illuminate\Auth\Listeners\SendEmailVerificationNotification;
 use Illuminate\Foundation\Support\Providers\EventServiceProvider as ServiceProvider;
-use Illuminate\Support\Facades\Event;
 
 class EventServiceProvider extends ServiceProvider
 {
+    protected $observers = [
+        WashOrder::class => [WashOrderObserver::class],
+    ];
     /**
      * The event to listener mappings for the application.
      *
@@ -25,7 +30,7 @@ class EventServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
-        //
+        WashOrder::observe(WashOrderObserver::class);
     }
 
     /**
