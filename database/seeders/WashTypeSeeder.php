550d9fb4e8e60b7cef7c96c43a836bfe42cbab39
Sergio Lobaton <sergio.lobaton@endeandina.bo>
2023-08-15 08:52:54 -0400

    Add wash type seeder

--- database/seeders/WashTypeSeeder.php
+++ database/seeders/WashTypeSeeder.php
@@ -13,8 +13,6 @@ class WashTypeSeeder extends Seeder
      */
     public function run(): void
     {
-        $faker = \Faker\Factory::create();
-
         WashType::insert([
             ['name' => 'Enzima'],
             ['name' => 'Casual'],
