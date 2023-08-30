23c4e8c44336481c115c047f977ac1b28133ecad
abelopezpaniagua <78995406+abelopezpaniagua@users.noreply.github.com>
2023-08-30 10:10:47 -0400

    remove unnecesary properties

--- app/Http/Resources/ClothSizeResource.php
+++ app/Http/Resources/ClothSizeResource.php
@@ -17,8 +17,6 @@ class ClothSizeResource extends ApiResource
             'id'                    => $this->id,
             'name'                  => $this->name,
             'description'           => $this->description,
-            'wash_price'            => $this->wash_price,
-            'wash_special_price'    => $this->wash_special_price,
             'is_active'             => $this->is_active,
             'created_at'            => $this->created_at,
             'updated_at'            => $this->updated_at
