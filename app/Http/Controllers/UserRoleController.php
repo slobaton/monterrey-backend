10a86630b3ad5488a9f326422d8d0d1f55fcec57
Sergio Lobaton <sergio.lobaton@endeandina.bo>
2024-01-18 15:19:59 -0400

    Implement user role controller

---
+++ app/Http/Controllers/UserRoleController.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Http\Requests\RoleAssignmentRequest;
+use App\Http\Resources\RoleCollection;
+use App\Models\Role;
+use App\Models\User;
+use Illuminate\Http\JsonResponse;
+
+class UserRoleController extends Controller
+{
+    public function assignRoles(User $user, RoleAssignmentRequest $request): JsonResponse
+    {
+        $roleIds = $request->input('role_ids');
+
+        $user->roles()->sync($roleIds);
+
+        return $this->respondWithSuccess([
+            'message' => 'Roles successfully assigned',
+        ]);
+    }
+
+    public function getUserRoles(User $user): RoleCollection
+    {
+        $roles = $user->roles;
+        return new RoleCollection($roles);
+    }
+}
