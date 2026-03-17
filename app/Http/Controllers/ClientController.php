ffe1467318de841fc4422cba4cfdd30c7b0f55e7
slobaton <lobaton.0722@gmail.com>
2026-03-17 09:21:30 -0400

    feat(currency): remove boliviano conversion, use dollars only

--- app/Http/Controllers/ClientController.php
+++ app/Http/Controllers/ClientController.php
@@ -25,7 +25,7 @@ class ClientController extends Controller
         $this->middleware('role:' . Roles::ADMIN->value)
             ->only(['update', 'destroy']);
         $this->middleware('role:' . Roles::ADMIN->value . ',' . Roles::SECRETARY->value)
-            ->only(['getWashTypes', 'getEffects', 'getParameters', 'getAccountMovements', 'addPaymentMovement', 'addDiscountMovement', 'getCurrencyChangeRate']);
+            ->only(['getWashTypes', 'getEffects', 'getParameters', 'getAccountMovements', 'addPaymentMovement', 'addDiscountMovement']);
         $this->middleware('role:' . Roles::ADMIN->value . ',' . Roles::SECRETARY->value . ',' . Roles::RECEPTIONIST->value)
             ->only(['index', 'store', 'show']);
     }
@@ -205,14 +205,4 @@ class ClientController extends Controller
         return $this->respondWithSuccess();
     }
 
-    public function getCurrencyChangeRate(Request $request, Client $client)
-    {
-        $currencyRate = $client->getCurrencyRate();
-
-        $data = [
-            'value' => $currencyRate
-        ];
-
-        return $this->respondWithSuccess($data);
-    }
 }
